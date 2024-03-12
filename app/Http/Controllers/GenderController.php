f66b7d47f34b00199380f3ad51061dea172513bb
Ranz <ranzkurtle02@gmail.com>
2024-03-12 11:58:01 +0800

    intl commit

---
+++ app/Http/Controllers/GenderController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GenderController extends Controller
+{
+    // index
+    //show
+    //create
+    //store
+    //edit
+    //update
+    //delete
+    //destroy
+
+    public function index(){
+        return view('gender.index');
+    }
+
+    public function show(){
+
+    }
+
+    public function create(){
+        
+    }
+
+    public function store(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function delete(){
+
+    }
+
+    public function destroy(){
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/users', function(){
-    return view('user.index');
-});
+Route
